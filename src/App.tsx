bfd4f36fb30e40ae6302e33f73ead1df4792fea2
Zilvinas Bucys <zilbuc@gmail.com>
2019-07-25 23:30:22 +0300

    header component and links to home, shop, contact

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import { ShopPage } from './pages/shop/shop.component';
+import { Header } from './components/header/header.component';
 
 export const App: React.FC = (): JSX.Element => {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
